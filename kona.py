d99260559da12390b65a9cb2849351a58fc83956
cocher <ggg1000111@gmail.com>
2016-01-25 22:48:02 +0800

    refactor code

--- kona.py
+++ kona.py
@@ -14,7 +14,8 @@ unsafe = "http://konachan.com/"
 tags = []
 count = 0
 start_num = 1
-pages_range = 1
+end_num = 1
+index_page_url_list = []
 pic_url_list = []
 
 threads_num = 0
@@ -31,17 +32,41 @@ class ProgressBarThread(threading.Thread):
 
 class DownLoadThread(threading.Thread):
     """docstring for DownLoadThread"""
+    judge = true
 
-    def __init__(self):
+    def __init__(self, TorF):
         super(DownLoadThread, self).__init__()
+        self.judge = TorF
+
+    def run(self):
+        if judge:
+            pass
+        else:
+            self.GetPicURL()
+
+    def GetPicURL():
+        global index_page_url_list
+        lock = threading.Lock()
+        while(index_page_url_list):
+            lock.acquire()
+            url = index_page_url_list.pop()
+            lock.release()
+            items = ParseJson(url)
+            for single in items:
+                lock.acquire()
+                UpdatePicURLList(
+                    {'id': single['id'], 'url': single['file_url']})
+                lock.release()
 
 
 def InputSettings():
-    global site, tags, start_num, pages_range
+    global site, tags, start_num, end_num, threads_num
     print "=========Konachan Picture Downloader========="
+    ########################################
     # Check if the failed file exists
     # If exists, ask user if resume last download.
     # ADD Check() && Resume()
+    ########################################
     print "请输入tag, 多个tag请用空格隔开: "
     tag_temp = raw_input()
     tags = tag_temp.split(' ')
@@ -54,24 +79,44 @@ def InputSettings():
     print "请输入开始页码和页码范围, 数字间用空格隔开(例:1 100):"
     range_temp = raw_input().split(' ')
     start_num = (int)(range_temp[0])
-    pages_range = (int)(range_temp[1])
+    end_num = (int)(range_temp[1]) + start_num
+    print "请输入线程数量:"
+    threads_num = (int)(raw_input())
 
 
-def BuildIndexPageURL(index_num):
-    ct_page_URL = site + "post.json?" + \
-        "page=" + str(index_num) + "&tags="
-    for tag in tags:
-        ct_page_URL = ct_page_URL + "+" + tag
-    return ct_page_URL
+def BuildAllPagesURL():
+    global index_page_url_list
+    for index_num in range(start_num, end_num):
+        ct_page_URL = site + "post.json?" + "page=" + str(index_num) + "&tags="
+        for tag in tags:
+            ct_page_URL = ct_page_URL + "+" + tag
+        index_page_url_list.append(ct_page_URL)
+
+
+def ParseJson(index_page_url):
+    page_content = pubFunc.OpenPage(pubFunc.BuildRequestGet(index_page_url))
+    return json.loads(page_content)
+
+
+def UpdatePicURLList(dict):
+    global pic_url_list
+    pic_url_list.append(dict)
 
 
 def BuildThreads(threads_num):
-    threads = []
-    progress_bar = ProgressBarThread(len(pic_url_list))
-    threads.append(progress_bar)
-    for i in range(0, threads_num):
-        t = DownLoadThread()
-        threads.append(t)
-    return threads
+    pass
 
-InputSettings()
+
+def GetAllPicURL(self):
+    print "正在获取链接...",
+    print "OK!"
+
+
+def Download(self):
+    pass
+
+
+if __name__ == '__main__':
+    pass
+else:
+    pass
--- test.py
+++ test.py
@@ -1,11 +1,1 @@
 # -*- coding:utf-8 -*-
-
-a = 3
-print a
-
-
-def x():
-    global a
-    a = 4
-x()
-print a
